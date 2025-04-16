9a851372b9629d613f6b3d83390852f13772db43
wwelden <wwelden@trinity.edu>
2025-04-16 17:07:32 -0500

    dark mode improvements

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/Portfolio/',
+  build: {
+    assetsInlineLimit: 0,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
